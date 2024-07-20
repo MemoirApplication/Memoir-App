f8200f97879c615e15d360b9e1c9dbd549beb6d2
Chikobara <albaraa.as.2002@gmail.com>
2024-07-20 06:28:04 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/app/(marketing)/_components/heading.tsx
    	modified:   src/app/(marketing)/page.tsx

--- src/app/(marketing)/_components/heading.tsx
+++ src/app/(marketing)/_components/heading.tsx
@@ -1,11 +1,19 @@
 "use client";
+import React from "react";
+import {Button} from "@nextui-org/react";
 
-
-export const Heading = () => { 
-    return (
-        <div className="max-w-3xl space-y-4">
-            <h1 className="text-4xl font-bold">Welcome to Memoir</h1>
-            <h1> Your Ideas, Documents and Plans all together in one place.. </h1>
-        </div>
-    )
-}
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-7xl font-bold">
+        Welcome to <span className="underline">Memoir</span>
+      </h1>
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents and Plans all together in one place.
+      </h1>
+      <Button color="primary" variant="shadow">
+        Test
+      </Button>
+    </div>
+  );
+};
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import { Heading } from "./_components/heading";
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 pb-10 px-6">
+      <div className="flex flex-col items-center justify-center md:justify-start   text-center gap-y-8 flex-1 pb-10 px-6">
           <Heading/>
       </div> 
     </div> 
