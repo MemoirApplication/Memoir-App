d60e00a7fe48a96d1e0fbac2a5daf4a7aa9d8412
Chikobara <albaraa.as.2002@gmail.com>
2024-07-22 03:05:55 +0300

    On branch main
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/providers.tsx
    	new file:   src/components/ThemeSwitcher.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,10 +2,14 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function Providers({
-  children
-}: { children: React.ReactNode }
-) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
