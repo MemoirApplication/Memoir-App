f4db51462a84a6121b1fad88543c0b1b69000bb7
Chikobara <albaraa.as.2002@gmail.com>
2024-07-27 10:22:09 +0300

    Made navbar with userbutton On branch main
    Changes to be committed:
    	modified:   src/app/(main)/(routes)/documents/page.tsx
    	new file:   src/app/(main)/_components/blocksnote.tsx

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -1,16 +1,48 @@
 "use client";
 
-import "@blocknote/core/fonts/inter.css";
-import { useCreateBlockNote } from "@blocknote/react";
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/mantine/style.css";
-
-import { Card, CardBody } from "@nextui-org/card";
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  Avatar,
+  DropdownMenu,
+  DropdownItem,
+  Card,
+  CardBody,
+} from "@nextui-org/react";
+import App from "../../_components/blocksnote";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 export default function Documents() {
+  const { user } = useUser();
   return (
-    <div className="h-screen min-h-full w-screen">
-      <div></div>
-    </div>
+    <>
+      <div className="pl-6 h-screen min-h-full flex flex-col w-screen">
+        {/*  */}
+
+        {/* navbar div */}
+        <div className="">
+          <Navbar position="static" isBordered={true}>
+            <NavbarContent as="div" justify="end">
+              <UserButton />
+            </NavbarContent>
+          </Navbar>
+        </div>
+
+        {/*  */}
+        <div>
+          <div className="h-full flex  flex-col justify-center items-center space-y-4">
+            <h2>Hey {user?.firstName}, Welcome to Memroir</h2>
+          </div>
+          {/* <App /> */}
+        </div>
+      </div>
+    </>
   );
 }
