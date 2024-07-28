e55d1ebbfb0e7f0dbeaae11b3dc125567f8d7853
Chikobara <albaraa.as.2002@gmail.com>
2024-07-28 06:45:47 +0300

    Listing the documents in the sidebar with Expanding functions and recursive calling to render the parentsDocument

--- src/app/(main)/_components/item.tsx
+++ src/app/(main)/_components/item.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { cn } from "@nextui-org/theme";
 import { Skeleton } from "@nextui-org/skeleton";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -29,11 +33,32 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const create = useMutation(api.documents.create);
+  const router = useRouter();
+
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     event.stopPropagation();
     onExpand?.();
+  };
+
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = create({ title: "Untitled", parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        router.push(`/documents/${documentId}`);
+      }
+    );
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
   };
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
@@ -69,6 +94,18 @@ export const Item = ({
         <kbd className="ml-auto pointer-events-none inline-flex select-none h-5 items-center rounded border  bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs pr-1">⌘ K</span>
         </kbd>
+      )}
+      {/* this is for plus icon nextto each page button  */}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
