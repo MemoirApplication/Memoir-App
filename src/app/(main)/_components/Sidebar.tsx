9965f9174351d9fad8d233042b6218e9b5224922
nemuirei <baraa.dou@gmail.com>
2024-07-28 03:32:51 +0300

    first fix for sidebar and navbar layout

--- src/app/(main)/_components/Sidebar.tsx
+++ src/app/(main)/_components/Sidebar.tsx
@@ -30,95 +30,91 @@ export const Sidebar = () => {
       error: "Failed to create a new note.",
     });
   };
-  return (
-    <>
-      <aside className=" h-screen  w-80 ">
-        <Card className="h-full ">
-          <div className="p-4 h-full flex-col ">
-            <Card isBlurred shadow="lg" className="mb-4">
-              <CardBody className="flex-row items-center justify-center">
-                <UserButton />
 
-                <p className="ml-2 select-none">Workspace</p>
-                <Button
-                  isIconOnly
-                  size="sm"
-                  variant="light"
-                  color="secondary"
-                  className="ml-auto"
-                >
-                  <ChevronDown />
-                </Button>
-              </CardBody>
-            </Card>
-            <div>
-              <Card>
-                <CardBody className="flex space-y-2">
+  return (
+    <aside className="fixed top-0 left-0 h-full w-60 z-50">
+      <Card className="h-full shadow-lg">
+        <div className="p-4 h-full flex flex-col">
+          <Card isBlurred shadow="lg" className="mb-4">
+            <CardBody className="flex-row items-center justify-center">
+              <UserButton />
+              <p className="ml-2 select-none">Workspace</p>
+              <Button
+                isIconOnly
+                size="sm"
+                variant="light"
+                color="secondary"
+                className="ml-auto"
+              >
+                <ChevronDown />
+              </Button>
+            </CardBody>
+          </Card>
+          <div>
+            <Card>
+              <CardBody className="flex flex-col space-y-2">
                   {/* <Button variant="light" className="justify-start">
                     <SearchIcon size={16} />
                     <p className="select-none">Search</p>
                   </Button> */}
-                  <Button
-                    onClick={() => router.push("/documents")}
-                    variant="light"
-                    className="justify-start"
-                  >
-                    <HomeIcon size={16} />
-                    <p className="select-none">Home</p>
-                  </Button>
-                  <Button variant="light" className="justify-start">
-                    <Calendar size={16} />
-                    <p className="select-none">Calendar</p>
-                  </Button>
-                  <Button variant="light" className="justify-start">
-                    <Settings2Icon size={16} />
-                    <p className="select-none">Settings</p>
-                  </Button>
-                </CardBody>
-              </Card>
-            </div>
-
-            <Divider className="my-4" />
-
-            <Card>
-              <CardBody>
-                {/* <Button onClick={onCreate} variant="flat" color="secondary">
-                  <PlusCircle size={18} />
-                  New Note
-                </Button> */}
-                <div className="space-y-1">
-                  <Item
-                    label="Search"
-                    icon={Search}
-                    isSearch
-                    onClick={() => {}}
-                  />
-                  <Item
-                    onClick={handleCreate}
-                    label="New Page"
-                    icon={PlusCircle}
-                  />
-                </div>
+                <Button
+                  onClick={() => router.push("/documents")}
+                  variant="light"
+                  className="justify-start"
+                >
+                  <HomeIcon size={16} />
+                  <p className="select-none">Home</p>
+                </Button>
+                <Button variant="light" className="justify-start">
+                  <Calendar size={16} />
+                  <p className="select-none">Calendar</p>
+                </Button>
+                <Button variant="light" className="justify-start">
+                  <Settings2Icon size={16} />
+                  <p className="select-none">Settings</p>
+                </Button>
               </CardBody>
             </Card>
-            <div className="absolute inset-x-0 bottom-0 m-4">
-              <Divider className="my-4" />
-              <Card shadow="lg">
-                <CardBody>
-                  <Button
-                    variant="shadow"
-                    color="secondary"
-                    className="justify-start "
-                  >
-                    <Trash2Icon size={16} />
-                    Trash
-                  </Button>
-                </CardBody>
-              </Card>
-            </div>
           </div>
-        </Card>
-      </aside>
-    </>
+          <Divider className="my-4" />
+          <Card>
+            <CardBody>
+              {/* <Button onClick={onCreate} variant="flat" color="secondary">
+                <PlusCircle size={18} />
+                New Note
+              </Button> */}
+              <div className="space-y-1">
+                <Item  
+                  label="Search" 
+                  icon={Search} 
+                  isSearch 
+                  onClick={() => {}} 
+                />
+                <Item
+                  onClick={handleCreate}
+                  label="New Page"
+                  icon={PlusCircle}
+                />
+              </div>
+            </CardBody>
+          </Card>
+          <div className="mt-auto">
+            <Divider className="my-4" />
+            <Card shadow="lg">
+              <CardBody>
+                <Button
+                  variant="shadow"
+                  color="secondary"
+                  className="justify-start"
+                >
+                  <Trash2Icon size={16} />
+                  Trash
+                </Button>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </Card>
+    </aside>
   );
 };
