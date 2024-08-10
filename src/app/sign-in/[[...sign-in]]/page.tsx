d982da2e8e5ee87048e8706bed93a18dead48149
LUCIFER1144 <135417247+LUCIFER1144@users.noreply.github.com>
2024-08-10 20:47:51 +0300

    Create page.tsx
    create beta version of the sign in /up page

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,70 @@
+"use client"
+import React from "react";
+import { Tabs, Tab, Input, Link, Button, Card, CardBody } from "@nextui-org/react";
+
+export default function App() {
+  const [selected, setSelected] = React.useState<string | number | null>("sign-in");
+
+  return (
+    // Use Flexbox to center the content
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-[340px] h-[400px]">
+        <CardBody className="overflow-hidden">
+          <Tabs
+            fullWidth
+            size="md"
+            aria-label="Tabs form"
+            selectedKey={selected}
+            onSelectionChange={(key) => setSelected(key)}
+          >
+            <Tab key="sign-in" title="Sign in">
+              <form className="flex flex-col gap-4">
+                <Input isRequired label="Email" placeholder="Enter your email" type="email" />
+                <Input
+                  isRequired
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                />
+                <p className="text-center text-small">
+                  Need to create an account?{" "}
+                  <Link size="sm" onPress={() => setSelected("sign-up")}>
+                    Sign up
+                  </Link>
+                </p>
+                <div className="flex gap-2 justify-end">
+                  <Button fullWidth color="primary">
+                    Sign in
+                  </Button>
+                </div>
+              </form>
+            </Tab>
+            <Tab key="sign-up" title="Sign up">
+              <form className="flex flex-col gap-4 h-[300px]">
+                <Input isRequired label="Name" placeholder="Enter your name" type="text" />
+                <Input isRequired label="Email" placeholder="Enter your email" type="email" />
+                <Input
+                  isRequired
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                />
+                <p className="text-center text-small">
+                  Already have an account?{" "}
+                  <Link size="sm" onPress={() => setSelected("sign-in")}>
+                    Sign in
+                  </Link>
+                </p>
+                <div className="flex gap-2 justify-end">
+                  <Button fullWidth color="primary">
+                    Sign up
+                  </Button>
+                </div>
+              </form>
+            </Tab>
+          </Tabs>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
