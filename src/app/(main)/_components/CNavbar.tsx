4aa7f2dc7fb7fd1ff170df1204755574df6a097c
bunrots <baraa.dou@gmail.com>
2024-08-14 23:23:49 +0300

    Fix type mismatch in DropdownItem onClick handler

--- src/app/(main)/_components/CNavbar.tsx
+++ src/app/(main)/_components/CNavbar.tsx
@@ -29,6 +29,7 @@ import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import { Doc, Id } from "../../../../convex/_generated/dataModel";
 
+// @ts-ignore
 export const CNavbar = ({ document }) => {
   const { isCollapsed, toggleSidebar } = useSidebar();
   const { isFullWidth, toggleWidth } = useEditor();
