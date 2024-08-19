3a3e3bbf5e6d26d02c0da5ad3edda5a2a015be50
bunrots <baraa.dou@gmail.com>
2024-08-20 00:54:53 +0300

    Update landing page design

--- src/app/(landingPage)/layout.tsx
+++ src/app/(landingPage)/layout.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { ThemeProvider } from "next-themes";
 import { NavigationBar } from "./_components/navbar";
 const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <main className=" select-none h-max text-foreground bg-background min-h-full flex flex-col">
-        <NavigationBar />
-        {children}
-      </main>
+      <ThemeProvider>
+        <main className="violet-light dark:violet-dark select-none h-max text-foreground bg-background min-h-full flex flex-col">
+          <div className="fixed top-0 right-0 left-0 z-50">
+            <NavigationBar />
+          </div>
+          {children}
+        </main>
+      </ThemeProvider>
     </div>
   );
 };
