669c4962d29d264ebd4191e49cf64a5dc1277cc2
Chikobara <albaraa.as.2002@gmail.com>
2024-08-29 11:37:58 +0300

    New Graph in sidebar

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -95,7 +95,6 @@ export default function Documents() {
                 </Button>
               </Card>
             </div>
-
             {/* <Card
               isFooterBlurred
               radius="lg"
